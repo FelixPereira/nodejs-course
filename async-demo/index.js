6b5b35114f0cb40316adf51d56066f08d9010523
Félix Pereira <felixpereira538@gmail.com>
2022-06-17 19:18:27 +0200

    Implement promises to fetch user from database

--- async-demo/index.js
+++ async-demo/index.js
@@ -15,12 +15,51 @@ function displayCommits(commits) {
   console.log(commits);
 };
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from the database');
-    callback({ id, gitHubUsername: 'Felix' });
-  }, 2000);
+
+getUser(1)
+  .then(user => getRepos(user.gitHubUsername))
+  .then(repos => getCommits(repos))
+  .then(commit => console.log(commit))
+  .catch(error => console.log(error.message));
+
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a user from the database');
+      resolve({ id, gitHubUsername: 'Felix' });
+    }, 2000);
+  }); 
 };
+
+function getRepos(userName) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['repo1', 'repo2', 'repo3'])
+    }, 2000);
+  })
+};
+
+function getCommits(repos) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({desc: 'Commits desc'})
+    }, 2000);
+  })
+};
+
+
+function getProducts(dataBase, products) {
+  return new Promise((resolve, reject) => {
+    const products = dataBase.get(products);
+    resolve(products);
+  })
+}
+
+
+
+/* 
+USING CALLBACK FUNCTIONS 
 
 function getRepos(userName, callback) {
   setTimeout(() => {
@@ -33,3 +72,4 @@ function getCommits(repos, callback) {
     callback({desc: 'Commits desc'})
   }, 2000);
 }
+*/
