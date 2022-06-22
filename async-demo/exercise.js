d0801764c7a6dbf95fed428df3fad54e55da0cf2
Félix Pereira <felixpereira538@gmail.com>
2022-06-22 19:18:13 +0200

    Async and Await exercice from course

---
+++ async-demo/exercise.js
@@ -0,0 +1,60 @@
+
+getCustomer(1, (customer) => {
+  console.log('Customer: ', customer);
+  if (customer.isGold) {
+    getTopMovies((movies) => {
+      console.log('Top movies: ', movies);
+      sendEmail(customer.email, movies, () => {
+        console.log('Email sent...')
+      });
+    });
+  }
+});
+
+
+// Async 
+async function getCus() {
+  try{
+    const user = await getCustomer();
+    const topMovies = await getTopMovies();
+    const send = await sendEmail();
+  
+    console.log('User: ', user);
+    console.log('Top movies: ', topMovies);
+    console.log('Message: ', send);
+  } catch(error) {
+    console.log(error.message);
+  }
+}
+
+getCus();
+
+
+function getCustomer() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ 
+        id: 1, 
+        name: 'Mosh Hamedani', 
+        isGold: true, 
+        email: 'email' 
+      });
+    }, 4000);
+  })  
+}
+
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  })
+}
+
+function sendEmail() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Email sent...');
+    }, 4000);
+  })
+}
